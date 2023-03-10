1e91050c55acbcfc9ca2ba41063efb097b846259
Zhigang <wangzhigang1999@live.cn>
2023-03-10 17:06:23 +0800

    完善了统计信息

--- src/main/java/com/bupt/dailyhaha/Audit.java
+++ src/main/java/com/bupt/dailyhaha/Audit.java
@@ -2,45 +2,41 @@ package com.bupt.dailyhaha;
 
 import com.google.gson.Gson;
 import com.mongodb.client.MongoClient;
+import jakarta.annotation.PreDestroy;
 import jakarta.servlet.http.HttpServletResponse;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.bson.Document;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.time.Instant;
+import java.util.UUID;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Component
 @Aspect
-public class Audit {
+public class Audit implements CommandLineRunner {
     final MongoClient client;
 
     ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 10, 0, TimeUnit.HOURS, new LinkedBlockingQueue<>());
 
     public final static Gson gson = new Gson();
     final static long start = System.currentTimeMillis();
+    final static String uuid = UUID.randomUUID().toString();
 
     public String env;
 
 
     public Audit(MongoClient client) {
         this.client = client;
-
         env = System.getenv("env");
-        /*
-            记录启动时间，因为render不保证24小时运行;需要统计宕机的时间
-         */
-        Document document = new Document("startTimestamp", start);
-        var str = Instant.ofEpochMilli(start).atZone(java.time.ZoneId.of("Asia/Shanghai")).toString();
-        document.append("startAt", str).append("env", env);
-        client.getDatabase("shadiao").getCollection("up").insertOne(document);
     }
 
     @Pointcut("execution(* com.bupt.dailyhaha.Controller.*(..))")
@@ -76,10 +72,39 @@ public class Audit {
                     .append("status", status)
                     .append("time", end - start)
                     .append("timeStamp", System.currentTimeMillis())
-                    .append("env", env);
+                    .append("env", env)
+                    .append("uuid", uuid);
             client.getDatabase("shadiao").getCollection("audit").insertOne(document);
-
         });
         return proceed;
+    }
+
+    @Override
+    public void run(String... args) {
+        /*
+            记录启动时间，因为render不保证24小时运行;需要统计宕机的时间
+         */
+        Document document = new Document("startTimestamp", start);
+        var str = Instant.ofEpochMilli(start).atZone(java.time.ZoneId.of("Asia/Shanghai")).toString();
+        document.append("startAt", str).append("env", env).append("uuid", uuid);
+        client.getDatabase("shadiao").getCollection("up").insertOne(document);
+    }
+
+    @PreDestroy
+    public void exit() {
+        pool.shutdown();
+        Document document = new Document("endTimestamp", System.currentTimeMillis());
+        var str = Instant.ofEpochMilli(start).atZone(java.time.ZoneId.of("Asia/Shanghai")).toString();
+        document.append("endAt", str);
+
+        var duration = System.currentTimeMillis() - start;
+        // convert to hours
+        duration /= 1000.0 * 60 * 60;
+        document.append("alive", duration);
+
+        client.getDatabase("shadiao").getCollection("up").updateOne(
+                new Document("uuid", uuid),
+                new Document("$set", document)
+        );
     }
 }
