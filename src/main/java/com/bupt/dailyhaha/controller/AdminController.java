82f08a0091ce0c4b02717ced4e51a5f1bcd5c702
Zhigang <wangzhigang1999@live.cn>
2023-04-10 11:29:43 +0800

    优化结构，增加了置顶的功能

--- src/main/java/com/bupt/dailyhaha/controller/AdminController.java
+++ src/main/java/com/bupt/dailyhaha/controller/AdminController.java
@@ -3,15 +3,15 @@ package com.bupt.dailyhaha.controller;
 import com.bupt.dailyhaha.anno.AuthRequired;
 import com.bupt.dailyhaha.pojo.ResultData;
 import com.bupt.dailyhaha.pojo.ReturnCode;
+import com.bupt.dailyhaha.pojo.Submission;
 import com.bupt.dailyhaha.service.ReviewService;
 import com.bupt.dailyhaha.service.StatisticService;
 import com.bupt.dailyhaha.service.SubmissionService;
 import com.bupt.dailyhaha.service.SysConfig;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin")
@@ -44,6 +44,48 @@ public class AdminController {
     public ResultData<Boolean> release() {
         return !reviewService.release() ? ResultData.fail(ReturnCode.RC500) : ResultData.success(true);
     }
+
+    /**
+     * 验证token
+     */
+    @RequestMapping("/verify")
+    @AuthRequired
+    public ResultData<Boolean> verify() {
+        return ResultData.success(true);
+    }
+
+    /**
+     * 置顶
+     *
+     * @param hash hash
+     * @return ResultData
+     */
+    @PostMapping("/top/{hash}")
+    @AuthRequired
+    public ResultData<Boolean> top(@PathVariable("hash") int hash) {
+        return ResultData.success(sysConfig.addTop(hash));
+    }
+
+    /**
+     * 取消置顶
+     *
+     * @param hash hash
+     * @return ResultData
+     */
+    @DeleteMapping("/top/{hash}")
+    @AuthRequired
+    public ResultData<Boolean> unTop(@PathVariable("hash") int hash) {
+        return ResultData.success(sysConfig.removeTop(hash));
+    }
+
+    /**
+     * 获取置顶
+     */
+    @GetMapping("/top")
+    public ResultData<Set<Submission>> getTop() {
+        return ResultData.success(sysConfig.getTop());
+    }
+
 
     /**
      * 统计从00:00:00到现在的状态
--- src/main/java/com/bupt/dailyhaha/pojo/Sys.java
+++ src/main/java/com/bupt/dailyhaha/pojo/Sys.java
@@ -3,9 +3,13 @@ package com.bupt.dailyhaha.pojo;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Set;
+
 @Document("sys")
 @Data
 public class Sys {
     private String id = "sys";
     Boolean botUp = true;
+
+    Set<Submission> topSubmission = Set.of();
 }
