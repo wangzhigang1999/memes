a34a671e57368fb62ab3e36de35e9c4fe35799a7
Zhigang <wangzhigang1999@live.cn>
2023-10-08 20:12:15 +0800

    增加了RSS的接口

---
+++ src/main/java/com/bupt/dailyhaha/service/RSSItemService.java
@@ -0,0 +1,51 @@
+package com.bupt.dailyhaha.service;
+
+import com.bupt.dailyhaha.pojo.RSSItem;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+public class RSSItemService {
+    final MongoTemplate template;
+
+    public RSSItemService(MongoTemplate template) {
+        this.template = template;
+    }
+
+    public List<RSSItem> getByAuthorAndBoard(String author, String board) {
+        return getByMap(Map.of("author", author, "board", board), Set.of("description", "comments", "guid"));
+    }
+
+    public List<RSSItem> getByAuthor(String author) {
+        return getByMap(Map.of("author", author), Set.of("description", "comments", "guid"));
+    }
+
+    public List<RSSItem> getByBoard(String board) {
+        return getByMap(Map.of("board", board), Set.of("description", "comments", "guid"));
+    }
+
+    private List<RSSItem> getByMap(Map<String, String> map, Set<String> ignoreFields) {
+        Query query = new Query();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            query.addCriteria(Criteria.where(entry.getKey()).is(entry.getValue()));
+        }
+        for (String field : ignoreFields) {
+            query.fields().exclude(field);
+        }
+
+        // sort by date
+        query.with(Sort.by(Sort.Direction.DESC, "pubDate"));
+
+        query.limit(50);
+        template.getDb().getCollection("rssitem").createIndex(new org.bson.Document("title", "text"));
+        return template.find(query, RSSItem.class);
+    }
+
+}
