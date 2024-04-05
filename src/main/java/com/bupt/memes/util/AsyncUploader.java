1aebe7d88ef8820a202aa4f1cdf533b3b51ce9fb
Zhigang <wangzhigang1999@live.cn>
2024-04-05 23:34:01 +0800

    Add AsyncUploader class for asynchronous file upload

---
+++ src/main/java/com/bupt/memes/util/AsyncUploader.java
@@ -0,0 +1,26 @@
+package com.bupt.memes.util;
+
+import com.bupt.memes.service.Interface.Storage;
+import org.slf4j.Logger;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+public record AsyncUpload(MultipartFile file, Storage storage) implements Callable<String> {
+    final static Logger logger = org.slf4j.LoggerFactory.getLogger(AsyncUpload.class);
+
+    @Override
+    public String call() {
+        if (file == null) {
+            logger.warn("upload image failed, file is null");
+            return "";
+        }
+        try {
+            return storage.store(file.getBytes(), file.getContentType()).getUrl();
+        } catch (IOException e) {
+            logger.error("upload image failed", e);
+            return "";
+        }
+    }
+}
