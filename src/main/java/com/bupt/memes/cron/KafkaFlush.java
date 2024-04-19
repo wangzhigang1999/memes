fac29506c39fac8ac02b507a1fc9277185c13364
Zhigang <wangzhigang1999@live.cn>
2024-04-20 00:24:06 +0800

    Add scheduling and lifecycle methods to KafkaFlush class

--- src/main/java/com/bupt/memes/cron/KafkaFlush.java
+++ src/main/java/com/bupt/memes/cron/KafkaFlush.java
@@ -1,2 +1,22 @@
-package com.bupt.memes.cron;public class KafkaFlush {
+package com.bupt.memes.cron;
+
+import com.bupt.memes.util.KafkaUtil;
+import jakarta.annotation.PreDestroy;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class KafkaFlush {
+    @Scheduled(fixedRate = 5000)
+    public void flush() {
+        KafkaUtil.flush();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        KafkaUtil.close();
+    }
+
 }
--- src/main/java/com/bupt/memes/util/KafkaUtil.java
+++ src/main/java/com/bupt/memes/util/KafkaUtil.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class KafkaUtil {
 
@@ -19,11 +20,25 @@ public class KafkaUtil {
 
     private volatile static KafkaProducer<String, byte[]> producer;
 
+    private static final AtomicBoolean alive = new AtomicBoolean(true);
+
     static {
-        PROPS.put("bootstrap.servers", System.getenv("BOOTSTRAP_ENDPOINT"));
+
+        try {
+            init();
+        } catch (Exception e) {
+            logger.error("Kafka producer init failed", e);
+        }
+    }
+
+    @SneakyThrows
+    private static void init() {
         PROPS.put("sasl.mechanism", "SCRAM-SHA-512");
         PROPS.put("security.protocol", "SASL_SSL");
+        PROPS.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        PROPS.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
 
+        PROPS.put("bootstrap.servers", System.getenv("BOOTSTRAP_ENDPOINT"));
         var username = System.getenv("UPSTASH_KAFKA_USERNAME");
         var password = System.getenv("UPSTASH_KAFKA_PASSWORD");
 
@@ -33,14 +48,7 @@ public class KafkaUtil {
                 password="%s";
                 """.formatted(username, password);
         PROPS.put("sasl.jaas.config", saslJaasConfig);
-        PROPS.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        PROPS.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-        init();
-        logger.warn(saslJaasConfig);
-        logger.info("Kafka producer init success");
-    }
 
-    private static void init() {
         if (producer == null) {
             synchronized (KafkaUtil.class) {
                 if (producer == null) {
@@ -48,10 +56,15 @@ public class KafkaUtil {
                 }
             }
         }
+        logger.info("Kafka producer init success");
     }
 
     @SneakyThrows
     public static void send(MediaMessage message) {
+        if (!alive.get()) {
+            logger.error("Kafka producer is not alive,maybe the producer is closed");
+            return;
+        }
         producer.send(new ProducerRecord<>(TOPIC, message.toByteArray()));
     }
 
@@ -74,6 +87,26 @@ public class KafkaUtil {
                 .build();
         send(message);
     }
+
+
+    public static void flush() {
+        if (producer == null) {
+            logger.error("Kafka producer is null, maybe the producer is not init");
+            return;
+        }
+        producer.flush();
+    }
+
+    public static void close() {
+        logger.warn("Kafka producer is closing.............");
+        boolean compared = alive.compareAndSet(true, false);
+        if (!compared || producer == null) {
+            return;
+        }
+        flush();
+        producer.close();
+    }
+
 
     public static void main(String[] args) {
         MediaMessage message = MediaMessage.newBuilder()
