2cc6b219c90d2687b776fc44d3ee8cde7abe78a6
Zhigang <wangzhigang1999@live.cn>
2024-06-10 12:50:02 +0800

    update log in EmbeddingUtil

--- src/main/java/com/bupt/memes/util/EmbeddingUtil.java
+++ src/main/java/com/bupt/memes/util/EmbeddingUtil.java
@@ -44,22 +44,26 @@ public class EmbeddingUtil {
                 .withRemoveEnabled()
                 .build();
 
+        var count = 0;
         for (Embedding embedding : embeddings.getDataList()) {
             newIndex.add(HNSWItem.builder().id(embedding.getId()).vector(toFloatArray(embedding)).build());
+            if (++count % 5000 == 0) {
+                logger.info("Indexed {} embeddings", count);
+            }
         }
         logger.info("Index built, saving to disk...");
         String indexName = String.format("%d-%d.index", System.currentTimeMillis(), embeddings.getCount());
         newIndex.save(new File(indexName));
-        logger.info("Index saved to " + indexName);
+        logger.info("Index saved to {}", indexName);
 
         // load index
         HnswIndex<String, float[], HNSWItem, Float> loadedIndex = HnswIndex.load(new File(indexName));
-        logger.info("Index loaded from " + indexName);
+        logger.info("Index loaded from {}", indexName);
         // search
         String queryId = embeddings.getData(0).getId();
         Embedding query = embeddings.getData(0);
-        logger.info("Searching for " + queryId);
-        loadedIndex.findNearest(toFloatArray(query), 10).forEach(item -> System.out.println(item.item().getId() + " " + item.distance()));
+        logger.info("Searching for {}", queryId);
+        loadedIndex.findNearest(toFloatArray(query), 10).forEach(System.out::println);
         logger.info("Search done, bye~");
     }
 
