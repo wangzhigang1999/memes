dbbc1fdcaad71dd5ed054aa707b190002032e00f
Zhigang <wangzhigang1999@live.cn>
2024-06-30 15:00:34 +0800

    bug fixed

--- src/main/java/com/memes/service/StatisticService.java
+++ src/main/java/com/memes/service/StatisticService.java
@@ -9,11 +9,14 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 import static com.memes.util.TimeUtil.getTodayStartUnixEpochMilli;
 
@@ -48,7 +51,7 @@ public class StatisticService {
         });
 
         Future<List<Map>> uuidCountMap = executorService.submit(() -> executeAggregation(start, end, "uuid"));
-        Future<List<Map>> urlCountMap = executorService.submit(() -> executeAggregation(start, end, "url"));
+        Future<List<Map>> urlCountMap = executorService.submit(() -> executeAggregation(start, end, "url", "method"));
 
         return Map.of(
                 "reqNumber", total.get(),
@@ -59,19 +62,38 @@ public class StatisticService {
     }
 
     @SuppressWarnings("rawtypes")
-    private List<Map> executeAggregation(long start, long end, String groupBy) {
+    private List<Map> executeAggregation(long start, long end, String... groupBy) {
         Aggregation aggregation = Aggregation.newAggregation(
                 Aggregation.match(Criteria.where("timestamp").gte(start).lte(end)),
                 Aggregation.group(groupBy)
                         .count().as("count")
-                        .avg("timecost").as("avgTimecost")
-                        .max("timecost").as("maxTimecost")
-                        .min("timecost").as("minTimecost")
+                        .avg("timecost").as("avg")
+                        .max("timecost").as("max")
+                        .min("timecost").as("min")
                         .first("timestamp").as("firstTime")
                         .last("timestamp").as("lastTime"),
                 Aggregation.sort(Sort.Direction.DESC, "count"));
 
-        return template.aggregate(aggregation, LogDocument.class, Map.class).getMappedResults();
+        return template.aggregate(aggregation, LogDocument.class, Map.class).getMappedResults().stream().map(StatisticService::flattenMap)
+                .collect(Collectors.toList());
+
+    }
+
+    public static Map<String, Object> flattenMap(Map<String, Object> map) {
+        return map.entrySet().stream()
+                .flatMap(entry -> flattenEntry(entry).entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private static Map<String, Object> flattenEntry(Map.Entry<String, Object> entry) {
+        Map<String, Object> result = new HashMap<>();
+        String newPrefix = entry.getKey();
+        if (entry.getValue() instanceof Map) {
+            ((Map<?, ?>) entry.getValue()).forEach((key, value) -> result.putAll(flattenEntry(new AbstractMap.SimpleEntry<>(key.toString(), value))));
+        } else {
+            result.put(newPrefix, entry.getValue());
+        }
+        return result;
     }
 
 }
