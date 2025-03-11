6ef4fe035024f6c5ab45c6dbfc79b4867e4eabc0
Zhigang <wangzhigang1999@live.cn>
2025-03-11 23:48:33 +0800

    refactor(config): 重构配置类并添加 Lombok 注解
    - 在 AppConfig 中添加 @Data 注解，简化代码并提高可维护性
    - 更新导入语句，优化代码结构
    - 在 MetricsConfig 和 WebMvcConfig 中调整导入顺序，提高代码可读性

--- src/main/java/com/memes/config/AppConfig.java
+++ src/main/java/com/memes/config/AppConfig.java
@@ -2,8 +2,9 @@ package com.memes.config;
 
 import com.google.gson.Gson;
 import com.memes.annotation.DynamicConfig;
-import com.memes.model.pojo.ConfigItem;
+import com.memes.model.pojo.Config;
 import com.memes.model.pojo.Submission;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -13,24 +14,24 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
- * 应用配置，动态更新
- * 简单的配置条目使用注解标记字段，复杂的配置条目使用注解标记方法
+ * 应用配置，动态更新 简单的配置条目使用注解标记字段，复杂的配置条目使用注解标记方法
  */
 @Slf4j
 @Component("appConfig")
+@Data
 public class AppConfig {
-    @DynamicConfig(key = "bot.up", desc = "机器人是否开启", defaultValue = "true", type = ConfigItem.Type.BOOLEAN)
+    @DynamicConfig(key = "bot.up", desc = "机器人是否开启", defaultValue = "true", type = Config.Type.BOOLEAN)
     public boolean botUp;
     @DynamicConfig(key = "submission.num.min", desc = "每天的最少投稿数", defaultValue = "50")
     public int minSubmissions = 50;
     @DynamicConfig(key = "submission.fetch.limit", desc = "每次获取的最大投稿数", defaultValue = "20")
     public int subFetchLimit = 20;
-    @DynamicConfig(key = "server.down", desc = "服务器是否停止服务", defaultValue = "false", type = ConfigItem.Type.BOOLEAN)
+    @DynamicConfig(key = "server.down", desc = "服务器是否停止服务", defaultValue = "false", type = Config.Type.BOOLEAN)
     public boolean serverDown = false;
 
     public Set<Submission> topSubmissions = new CopyOnWriteArraySet<>();
 
-    @DynamicConfig(key = "top.submission", desc = "置顶的投稿", defaultValue = "[]", type = ConfigItem.Type.JSON, visible = false)
+    @DynamicConfig(key = "top.submission", desc = "置顶的投稿", defaultValue = "[]", type = Config.Type.JSON, visible = false)
     public void setTopSubmissions(String topSubmissions) {
         if (topSubmissions == null || topSubmissions.isEmpty()) {
             return;
@@ -41,7 +42,7 @@ public class AppConfig {
 
     public Set<String> uidBlacklist = new CopyOnWriteArraySet<>();
 
-    @DynamicConfig(key = "blacklist", desc = "黑名单", defaultValue = "[]", type = ConfigItem.Type.JSON, visible = false)
+    @DynamicConfig(key = "blacklist", desc = "黑名单", defaultValue = "[]", type = Config.Type.JSON, visible = false)
     public void setUidBlacklist(String uidBlacklist) {
         if (uidBlacklist == null || uidBlacklist.isEmpty()) {
             return;
